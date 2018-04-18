c5311bda2f2a0a73383178a4d2f79a2e9a737c28
zacky <kic.yuuki@gmail.com>
2018-04-18 19:22:37 -0400

    edit page add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -12,6 +16,16 @@ class ArticlesController < ApplicationController
     else
       # 記事の保存に失敗
       render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
   
