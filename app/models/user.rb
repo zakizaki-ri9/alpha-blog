3ad11030a5d2a13801af8309d8991f243c6d7348
zacky <kic.yuuki@gmail.com>
2018-04-29 22:45:15 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   
+  # usernameのバリデーションチェック定義
+  validates :username,
+      presence: true,
+      uniqueness: { case_sensitive: false},
+      length: {minimum: 3, maximum: 25}
+  
+  # email用の正規表現定義
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  # emailのバリデーションチェック定義
+  validates :email,
+      presence: true,
+      length: {maximum: 105},
+      uniqueness: { case_sensitive: false},
+      format: { with: VALID_EMAIL_REGEX }
 end
