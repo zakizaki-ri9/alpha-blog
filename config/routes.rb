2ead31508fcba0e7b521e10f004c20483f21273b
zacky <kic.yuuki@gmail.com>
2018-05-01 19:17:10 -0400

    add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get 'about', to: 'page#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+resources :users, except: [:new]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
