a8a508be61cd68d103bc9be10fb3408820d49c0b
zacky <kic.yuuki@gmail.com>
2018-05-04 21:42:55 -0400

    add login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    # if session[:user_id]の判定結果が返却される
+    # !! はBool値を返却させるための処置
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "Yout must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show]
+  
   def index
     # 3ユーザーずつ ページング処理
     @users = User.paginate(page: params[:page], per_page: 3)
@@ -20,11 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   
@@ -43,4 +42,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :password)
     end
   
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
