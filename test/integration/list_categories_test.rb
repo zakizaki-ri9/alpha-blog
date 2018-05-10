d813786b52a8cac57c255b0d834c89acb95f3d91
zacky <kic.yuuki@gmail.com>
2018-05-10 18:47:38 -0400

    add index page test code

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @category = Category.create(name: "test1")
+    @category2 = Category.create(name: "test2")
+  end
+  
+  # indexページにsetupで生成したカテゴリのページのURLが表示されることの悪人
+  test "should show categories listing" do
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+end 
