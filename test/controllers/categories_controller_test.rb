d26aca403c58b8705e884031eebce7c79602660e
zacky <kic.yuuki@gmail.com>
2018-05-13 18:50:58 -0400

    add test code for category admin only

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @c = Category.create(name: "test")
+    @user = User.create(username: "ccc", email: "ccc@a.com", password: "ccc", admin: true)
   end
   
   test "should get categories index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -19,4 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
     get(:show, {'id' => @c.id})
     assert_response :success
   end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "test"}
+    end
+    assert_redirected_to categories_path
+  end
+  
 end
