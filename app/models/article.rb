0b6aeb05fc77f2c54767309d10ec3aa57359bdf6
zacky <kic.yuuki@gmail.com>
2018-05-15 18:57:11 -0400

    add articlecategory table

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ActiveRecord::Base
   
   belongs_to :user
   
+  # 記事とカテゴリの紐付け管理
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   # presence = nilチェック
   # length = 長さチェック
   validates :title, presence: true, length: {minimus: 3, maximum: 50}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507232123) do
+ActiveRecord::Schema.define(version: 20180515224248) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
